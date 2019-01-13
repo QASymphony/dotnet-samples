eeda7620fbc433373037425ed177c8de7f2a8f3e
Tam Vo <tamvo@qasymphony.com>
2019-01-13 14:46:10 +0700

    add unit test for MStest project and NUnit

---
+++ NUnitTestSample/UnitTest/Arithmetic.cs
@@ -0,0 +1,25 @@
+﻿namespace NUnitTestSample.UnitTest
+{
+    class Arithmetic
+    {
+        public decimal Addition(int x, int y)
+        {
+            return x + y;
+        }
+
+        public decimal Subtraction(int x, int y)
+        {
+            return x - y;
+        }
+
+        public decimal Multiplication(int x, int y)
+        {
+            return x * y;
+        }
+
+        public decimal Division(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
