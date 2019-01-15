1b37845f613cb492eafb9e369216c13b129c0b59
Huy Nguyen <huynguyen@qasymphony.com>
2019-01-15 13:25:56 -0500

    refactored

--- DotnetCore/xUnitSample/BrowserTest.cs
+++ DotnetCore/xUnitSample/BrowserTest.cs
@@ -26,27 +26,20 @@ namespace xUnitSample
         [Fact]
         public void SearchForQtestLaunch()
         {
-            try
-            {
-                _webDriver.Navigate().GoToUrl("https://www.qasymphony.com");
+            _webDriver.Navigate().GoToUrl("https://www.qasymphony.com");
 
-                // click on Search icon to open search input
-                IWebElement searchIcon = _webDriver.FindElement(By.XPath("/html/body/div[2]/header/div/div[2]/div/nav/ul/li[9]/a"));
-                searchIcon.Click();
+            // click on Search icon to open search input
+            IWebElement searchIcon = _webDriver.FindElement(By.XPath("/html/body/div[2]/header/div/div[2]/div/nav/ul/li[9]/a"));
+            searchIcon.Click();
 
-                // find the search input element and search for `qTest Launch`
-                IWebElement searchInput = _webDriver.FindElement(By.Id("input-search"));
-                searchInput.SendKeys("qTest Launch");
-                searchInput.SendKeys(Keys.Enter);
+            // find the search input element and search for `qTest Launch`
+            IWebElement searchInput = _webDriver.FindElement(By.Id("input-search"));
+            searchInput.SendKeys("qTest Launch");
+            searchInput.SendKeys(Keys.Enter);
 
-                // from search result, look for h2 elament containing text qTest Launch, and click on it
-                IWebElement h2Element = _webDriver.FindElement(By.XPath("/html/body/div[2]/main/div/div/div[1]/a/div[2]/div/h2"));
-                h2Element.Click();
-            }
-            finally
-            {
-                _webDriver.Close();
-            }
+            // from search result, look for h2 elament containing text qTest Launch, and click on it
+            IWebElement h2Element = _webDriver.FindElement(By.XPath("/html/body/div[2]/main/div/div/div[1]/a/div[2]/div/h2"));
+            h2Element.Click();
         }
 
         /// <summary>
@@ -54,22 +47,15 @@ namespace xUnitSample
         /// </summary>
         [Fact]
         public void FailingTest()
-        {
-            try
-            {
-                _webDriver.Navigate().GoToUrl("https://www.qasymphony.com");
-                IWebElement element = _webDriver.FindElement(By.Id("Non-Existing Element"));
-                element.Click();
-            }
-            finally
-            {
-                _webDriver.Close();
-
-            }
+        { 
+            _webDriver.Navigate().GoToUrl("https://www.qasymphony.com");
+            IWebElement element = _webDriver.FindElement(By.Id("Non-Existing Element"));
+            element.Click();
         }
 
         public void Dispose()
         {
+            _webDriver.Close();
             _webDriver.Quit();
         }
     }
