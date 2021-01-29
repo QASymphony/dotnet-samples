aeea7b50ae1e6fcb71dac88c7a0f07e7e7ca2e5f
John Doe <huynguyen@qasymphony.com>
2021-01-29 15:28:55 -0500

    fixed failed test

--- DotnetCore/xUnitSample/BrowserTest.cs
+++ DotnetCore/xUnitSample/BrowserTest.cs
@@ -26,19 +26,19 @@ namespace xUnitSample
         [Fact]
         public void SearchForQtestLaunch()
         {
-            _webDriver.Navigate().GoToUrl("https://www.qasymphony.com");
+            _webDriver.Navigate().GoToUrl("https://www.google.com");
 
             // click on Search icon to open search input
-            IWebElement searchIcon = _webDriver.FindElement(By.XPath("/html/body/div[2]/header/div/div[2]/div/nav/ul/li[9]/a"));
-            searchIcon.Click();
+            //IWebElement searchIcon = _webDriver.FindElement(By.Name("q"));
+            //searchIcon.Click();
 
             // find the search input element and search for `qTest Launch`
-            IWebElement searchInput = _webDriver.FindElement(By.Id("input-search"));
+            IWebElement searchInput = _webDriver.FindElement(By.Name("q"));
             searchInput.SendKeys("qTest Launch");
             searchInput.SendKeys(Keys.Enter);
 
             // from search result, look for h2 elament containing text qTest Launch, and click on it
-            IWebElement h2Element = _webDriver.FindElement(By.XPath("/html/body/div[2]/main/div/div/div[1]/a/div[2]/div/h2"));
+            IWebElement h2Element = _webDriver.FindElement(By.LinkText("https://www.tricentis.com/resources/qtest-launch-test-automation-management/"));
             h2Element.Click();
         }
 
