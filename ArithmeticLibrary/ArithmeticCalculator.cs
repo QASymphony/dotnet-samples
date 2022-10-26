2add829d3e018f0581f6af56cd2abf4e0bf4fa2e
Peter Horváth <peter.horvath@sonarsource.com>
2022-10-26 10:47:01 +0200

    remove again

--- ArithmeticLibrary/ArithmeticCalculator.cs
+++ ArithmeticLibrary/ArithmeticCalculator.cs
@@ -1,22 +1,7 @@
 ﻿namespace ArithmeticLibrary
 {
     public class ArithmeticCalculator
-    {
-        public decimal Add(int x, int y)
-        {
-            return x + y;
-        }
-
-        public decimal Subtract(int x, int y)
-        {
-            return x - y;
-        }
-
-        public decimal Multiply(int x, int y)
-        {
-            return x * y;
-        }
-
+    {   
         public decimal Divide(int x, int y)
         {
             return x / y;
